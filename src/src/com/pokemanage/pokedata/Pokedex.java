f18341b0ca5c223405af071e49c5ad94e35497ea
LawrenceThibodeaux <daemonhouse@gmail.com>
2018-01-30 22:11:27 -0800

    Completed implementation of Pokedex and Pokemon

--- src/src/com/pokemanage/pokedata/Pokedex.java
+++ src/src/com/pokemanage/pokedata/Pokedex.java
@@ -3,6 +3,17 @@ package com.pokemanage.pokedata;
 import java.util.Map;
 
 public class Pokedex {
-    private Map<String, Boolean> posessionMap;
+    private final Map<String, Boolean> posessionMap;
 
+    public Pokedex(final Map<String, Boolean> posessionMap) {
+        this.posessionMap = posessionMap;
+    }
+
+    public Map<String, Boolean> posessionMap() {
+        return this.posessionMap;
+    }
+
+    public void capturePokemon(final String name) {
+        posessionMap.put(name, true);
+    }
 }
--- src/src/com/pokemanage/pokedata/Pokemon.java
+++ src/src/com/pokemanage/pokedata/Pokemon.java
@@ -3,6 +3,8 @@ package com.pokemanage.pokedata;
 import java.util.List;
 
 public class Pokemon {
+    public static final int OUT_OF_PC = 0;
+
     private String name;
     private String nickname;
     private int level;
@@ -30,5 +32,49 @@ public class Pokemon {
         this.isTraded = isTraded;
         this.hmMoves = hmMoves;
         this.originalTrainer = originalTrainer;
+    }
+
+    public String name() {
+        return this.name;
+    }
+
+    public String nickname() {
+        return this.nickname;
+    }
+
+    public int level() {
+        return this.level;
+    }
+
+    public int box() {
+        return this.box;
+    }
+
+    public int hp() {
+        return this.hp;
+    }
+
+    public boolean isTraded() {
+        return this.isTraded;
+    }
+
+    public List<HMMove> hmMoves() {
+        return this.hmMoves;
+    }
+
+    public PokeTrainer originalTrainer() {
+        return this.originalTrainer;
+    }
+
+    public void levelUp() {
+        this.level++;
+    }
+
+    public void putInBox(final int box) {
+        this.box = box;
+    }
+
+    public void takeOutOfPc() {
+        this.box = OUT_OF_PC;
     }
 }
