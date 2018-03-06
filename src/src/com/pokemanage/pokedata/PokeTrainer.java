0febbec27c6792ffd9fbe3a1fdd5f95560f29105
LawrenceThibodeaux <daemonhouse@gmail.com>
2018-03-05 21:02:20 -0800

    Implemented PokeTrainer, changed currentParty implementation from simple array to List (to be underlying ArrayList)

--- src/src/com/pokemanage/pokedata/PokeTrainer.java
+++ src/src/com/pokemanage/pokedata/PokeTrainer.java
@@ -1,13 +1,15 @@
 package com.pokemanage.pokedata;
 
+import java.util.List;
+
 public class PokeTrainer {
-    private Pokemon[] currentParty;
+    private List<Pokemon> currentParty;
     private PokemonQueue pokeQueue;
     private Pokedex pokedex;
-    private PokemonVersionColor version;
+    private final PokemonVersionColor version;
 
     public PokeTrainer(
-            final Pokemon[] currentParty,
+            final List<Pokemon> currentParty,
             final PokemonQueue pokeQueue,
             final Pokedex pokedex,
             final PokemonVersionColor version) {
@@ -15,5 +17,36 @@ public class PokeTrainer {
         this.pokeQueue = pokeQueue;
         this.pokedex = pokedex;
         this.version = version;
+    }
+
+    public List<Pokemon> currentParty() {
+        return this.currentParty;
+    }
+
+    public PokemonQueue pokeQueue() {
+        return this.pokeQueue;
+    }
+
+    public Pokedex pokedex() {
+        return this.pokedex;
+    }
+
+    public PokemonVersionColor version() {
+        return this.version;
+    }
+
+    public void trade(final PokeTrainer other, final Pokemon toGive, final Pokemon toReceive) {
+        if (!other.currentParty().contains(toReceive)) {
+            return;
+        }
+        this.currentParty.remove(toGive);
+        this.currentParty.add(toReceive);
+        other.currentParty().remove(toReceive);
+        other.currentParty().add(toGive);
+    }
+
+    public void addPokemon(final Pokemon pokemon) {
+        this.pokeQueue.enqueue(pokemon);
+        this.pokedex.capturePokemon(pokemon.name());
     }
 }
